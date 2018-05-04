4c543dd9e19211d3d5f56322662c054f102ad4a2
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-05-03 23:06:01 -0300

    Realizando pequenas alterações

--- pucrs.s2b.exemplo02/src/test/java/pucrs/s2b/exemplo02/AppTest.java
+++ pucrs.s2b.exemplo02/src/test/java/pucrs/s2b/exemplo02/AppTest.java
@@ -4,15 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-
-    /**
-     * Rigourous Test :-)
-     */
 	@Test
     public void testApp()
     {
