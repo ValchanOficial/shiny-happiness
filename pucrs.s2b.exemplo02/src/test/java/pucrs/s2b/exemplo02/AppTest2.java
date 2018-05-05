f7500d7168a391bf65df655c89f00cf8567922ff
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-05-05 16:14:29 -0300

    Verificando Exception Parameters

--- pucrs.s2b.exemplo02/src/test/java/pucrs/s2b/exemplo02/AppTest2.java
+++ pucrs.s2b.exemplo02/src/test/java/pucrs/s2b/exemplo02/AppTest2.java
@@ -1,10 +1,16 @@
 package pucrs.s2b.exemplo02;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import junitparams.FileParameters;
+import org.junit.runner.RunWith;
 
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
 public class AppTest2 {
-	/*	VERIFICAR CÓDIGO*/
+	/*	VERIFICAR CÓDIGO
+	 java.lang.IllegalArgumentException: Cannot parse parameters. Did you use ',' or '|' as column separator?
+	 */
 	@Test
 	@FileParameters("src/teste.csv")
 	public void paramTesteArquivo(int a, int b, int r){
